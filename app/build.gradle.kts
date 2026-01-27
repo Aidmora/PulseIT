b52334be2ef6d5f6d29e2d3e22c0848fd6983e2a
Ariel David Mora Jarrín <arielabc389@gmail.com>
2026-01-27 10:18:24 -0500

    Actualizando Master - Remote

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.material3)
+     implementation(libs.androidx.material3)
     implementation(libs.google.services)
     implementation(libs.firebase.bom)
     implementation(libs.firebase.auth.ktx)
